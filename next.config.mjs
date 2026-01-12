1d9be25ea0d326a7926ca0a478df1769e0bbe0c8
rapozoantonio <rapozoantonio@github.com>
2026-01-12 12:21:52 -0300

    fix build issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,23 @@
-import type { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60 * 60 * 24 * 365, // 1 year
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'miro.medium.com',
+        protocol: "https",
+        hostname: "miro.medium.com",
       },
       {
-        protocol: 'https',
-        hostname: 'drive.google.com',
+        protocol: "https",
+        hostname: "drive.google.com",
       },
     ],
-  },
-  experimental: {
-    optimizePackageImports: ['framer-motion', 'next-intl'],
   },
   compress: true,
   poweredByHeader: false,
